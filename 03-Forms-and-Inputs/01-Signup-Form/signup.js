f61d60cda1fd7b96c8513724437614e4bf26b14c
BlakeSandes <stellarshade@gmail.com>
2017-01-09 21:00:42 -0800

    I changed the names of all the index.html files in each of the form folders to a to a name reflecting the relevent form.  I added form validation to the JavaScript in the signup form, which also required changing the HMTL and CSS files to make it all work and look right.

---
+++ 03-Forms-and-Inputs/01-Signup-Form/signup.js
@@ -0,0 +1,157 @@
+var signupForm = document.getElementById('signup'),
+firstName = document.getElementById('first_name'),
+lastName = document.getElementById('last_name'),
+userName = document.getElementById('user_name'),
+email = document.getElementById('user_email'),
+datefield = document.getElementById('datefield'),
+month = document.getElementById('month'),
+day = document.getElementById('day'),
+year = document.getElementById('year'),
+password = document.getElementById('user_password'),
+confirmPassword = document.getElementById('confirm_password'),
+dateOfBirth;
+
+
+// change .invalid class to inputs if incorrect input.
+signupForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  if (validator.isEmpty(firstName.value) || firstName.value.length < 2) {
+    firstName.classList.add('invalid');
+    firstName.previousElementSibling.classList.add('error-icon');
+  }
+
+  if (validator.isEmpty(lastName.value) || firstName.value.length < 2) {
+    lastName.classList.add('invalid');
+    lastName.previousElementSibling.classList.add('error-icon');
+  }
+
+  if (validator.isEmpty(userName.value) || userName.value.length < 2) {
+    userName.classList.add('invalid');
+    userName.previousElementSibling.classList.add('error-icon');
+  }
+
+  if (!validator.isEmailAddress(email.value)) {
+    email.classList.add('invalid');
+    email.previousElementSibling.classList.add('error-icon');
+  }
+
+  if ( !validator.isEmpty(year.value) && !validator.isEmpty(month.value) && !validator.isEmpty(day.value) ) {
+    dateOfBirth = new Date(year.value, month.value - 1, day.value);
+  } 
+  if ( validator.isEmpty(year.value) || year.value.length !== 4 || isNaN(year.value)) {
+    year.classList.add('invalid');
+    year.previousElementSibling.classList.add('error-icon');
+  } 
+  if ( validator.isEmpty(month.value) || month.value.length !== 2 || isNaN(month.value)) {
+    month.classList.add('invalid');
+    month.previousElementSibling.classList.add('error-icon');
+  } 
+  if ( validator.isEmpty(day.value) || day.value.length !== 2 || isNaN(day.value) ) {
+    day.classList.add('invalid');
+    day.previousElementSibling.classList.add('error-icon');
+  }
+  
+  if (validator.isEmpty(password.value) || password.value.length < 6) {
+    password.classList.add('invalid');
+    password.previousElementSibling.classList.add('error-icon');
+  }
+
+  if (validator.isEmpty(confirmPassword.value) || confirmPassword.value.length < 6 || password.value !== confirmPassword.value ) {
+    confirmPassword.classList.add('invalid');
+    confirmPassword.previousElementSibling.classList.add('error-icon');
+  }
+}, false);
+
+
+// Remove .invalid class if correct input.
+
+firstName.addEventListener('input', function(event) {
+  event.preventDefault();
+
+  if (!validator.isEmpty(this.value) && this.value.length > 2) {
+    this.classList.remove('invalid');
+    this.previousElementSibling.classList.remove('error-icon');
+    this.previousElementSibling.classList.add('valid');
+  }
+}, false);
+
+lastName.addEventListener('input', function(event) {
+  event.preventDefault();
+
+  if (!validator.isEmpty(this.value) && this.value.length > 2) {
+   this.classList.remove('invalid');
+   this.previousElementSibling.classList.remove('error-icon');
+    this.previousElementSibling.classList.add('valid');
+  }
+}, false);
+
+userName.addEventListener('input', function(event) {
+  event.preventDefault();
+
+  if (!validator.isEmpty(this.value) && this.value.length > 2) {
+    this.classList.remove('invalid');
+    this.previousElementSibling.classList.remove('error-icon');
+    this.previousElementSibling.classList.add('valid');
+  }
+}, false);
+
+email.addEventListener('input', function (event) {
+  event.preventDefault();
+
+  if (validator.isEmailAddress(this.value)) {
+    this.classList.remove('invalid');
+    this.previousElementSibling.classList.remove('error-icon');
+    this.previousElementSibling.classList.add('valid');
+  }
+}, false);
+
+month.addEventListener('input', function(event) {
+  event.preventDefault();
+
+  if (this.value.length === 2 && !isNaN(this.value)) {
+    this.classList.remove('invalid');
+    this.previousElementSibling.classList.remove('error-icon');
+    this.previousElementSibling.classList.add('valid');
+  }
+}, false);
+
+day.addEventListener('input', function(event) {
+  event.preventDefault();
+
+  if (this.value.length === 2 && !isNaN(this.value)) {
+    this.classList.remove('invalid');
+    this.previousElementSibling.classList.remove('error-icon');
+    this.previousElementSibling.classList.add('valid');
+  }
+}, false);
+
+year.addEventListener('input', function(event) {
+  event.preventDefault();
+
+  if ( year.value.length === 4 && !isNaN(year.value)) {
+    this.classList.remove('invalid');
+    this.previousElementSibling.classList.remove('error-icon');
+    this.previousElementSibling.classList.add('valid');
+  }
+}, false);
+
+password.addEventListener('input', function(event) {
+  event.preventDefault();
+
+  if (this.value.length > 6) {
+    this.classList.remove('invalid');
+    this.previousElementSibling.classList.remove('error-icon');
+    this.previousElementSibling.classList.add('valid');  
+  }
+}, false);
+
+confirmPassword.addEventListener('input', function(event) {
+  event.preventDefault();
+
+  if (this.value === password.value) {
+    this.classList.remove('invalid');
+    this.previousElementSibling.classList.remove('error-icon');
+    this.previousElementSibling.classList.add('valid');  
+  }
+}, false);
---
+++ 03-Forms-and-Inputs/06-Shipping-and-Billing-Form/validator.js
@@ -0,0 +1,389 @@
+(function(window) {
+  var validator = {
+    isEmailAddress : function(input) {
+      // Grab index of @ symbol
+      var at = input.indexOf('@');
+      // Grab text before ampersand
+      var first = input.substr(0,at);
+      // Grab text after ampersand
+      var last = input.substr(at + 1);
+      
+      if (!input) {
+        throw "Please fill in email input please.";
+      }
+      // Check if String type
+      if (typeof first === 'string' && at >= 0 && typeof last === 'string') {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isPhoneNumber : function(input) {
+      var toNumber;
+      var phoneString;
+      // If input number is written as string, remove common parentheses and dashes and convert to number and use toString method to get number length.
+      if (typeof input === 'string') {
+        toNumber = input.replace(/[()-]/gi, "");
+        toNumber = parseInt(toNumber);
+        phoneString = toNumber.toString();
+      } else if (typeof input === 'number') {
+        // Use toString method to get number length.
+        phoneString = input.toString();
+      }
+
+      if (!input) {
+        throw "Please input your phone number please.";
+      }
+      // Return true if ten digits.
+      if (phoneString.length === 10) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    withoutSymbols : function(input) {
+      var symbollLess = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`'"~()]/gi," ");
+      return symbollLess;
+    },
+    isDate : function(input) {
+      // Returns true if new Date doesn't throw an "Invalid Date" error and if the date Object is a number.
+      return ( (new Date(input) !== "Invalid Date" && !isNaN(new Date(input)) ));
+    },
+    isBeforeDate : function(input, reference) {
+      var date1 = new Date(input);
+      var date2 = new Date(reference);
+
+      // Confirm that both dates are valid dates.
+      if ( (this.isDate(input) === false) || (this.isDate(reference) === false) ) {
+        throw "Invalid Date";
+      }
+      // Get each date in number form from year 1970 and compare dates.
+      if ( date1.getTime() > date2.getTime() ) {
+        return true;
+      } else {
+        return false;
+      }
+
+    },
+    isAfterDate : function(input, reference) {
+      var date1 = new Date(input);
+      var date2 = new Date(reference);
+
+
+      if ( (this.isDate(input) === false) || (this.isDate(reference) === false) ) {
+        throw "Invalid Date";
+      }
+      // Get each date in number form from year 1970 and compare dates. 
+      if ( date1.getTime() < date2.getTime() ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isBeforeToday : function(input) {
+      var today = new Date();
+      var testDate = new Date(input);
+      
+      if ( this.isDate(input) === false ) {
+        throw "Invalid Date";
+      }
+      // Get each date in number form from year 1970 and compare dates.
+      if (testDate.getTime() < today.getTime()) {
+        return true;
+      } else {
+        return false;
+      }
+
+    },
+    isAfterToday : function(input) {
+      var today = new Date();
+      var testDate = new Date(input);
+
+      if ( this.isDate(input) === false ) {
+        throw "Invalid Date";
+      }
+      // Get each date in number form from year 1970 and compare dates.
+      if (testDate.getTime() > today.getTime()) {
+        return true;
+      } else {
+        return false;
+      }
+
+    },
+    isEmpty : function(input) {
+      // Check if undefined or null.
+      if(input === undefined || input === null) {
+        return false;
+      }
+      // Return true if zero length or only white space. 
+      if (input.length === 0 || !input.trim()) {
+        return true;
+      } else  {
+        return false;
+      }
+    },
+    contains : function(input, words) {
+      // Remove symbols.
+      var noSymbol = this.withoutSymbols(input);
+      // Convert string to an array to check each word.
+      var inputArr = noSymbol.split(' ');
+      var match = [];
+      // Compare the words array within the input array for matches and push matches in match array.
+      for (var i = 0; i < words.length; i++) {
+        for (var j = 0; j < inputArr.length; j++) {
+          if (words[i] === inputArr[j]) {
+            match.push(words[i]);
+          }
+        }
+      }
+      // Return true if items in match array.
+      if (match.length > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    lacks : function(input, words) {
+      // Remove symbols.
+      var noSymbol = this.withoutSymbols(input);
+      // Convert string to an array to check each word.
+      var inputArr = noSymbol.split(' ');
+      var match = [];
+      // Compare the words array within the input array for matches and push matches in match array.
+      for (var i = 0; i < words.length; i++) {
+        for (var j = 0; j < inputArr.length; j++) {
+          if(words[i] === inputArr[j]) {
+            match.push(words[i])
+          }
+        }
+      }
+      // Return true if NO items in match array.
+      if (match.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isComposedOf : function(input, strings) {
+      strings = strings.sort(function(a,b) {
+        return b.length - a.length;
+      });
+      strings = strings.join('');
+      strings = this.withoutSymbols(strings.toLowerCase());
+      input = this.withoutSymbols(input.toLowerCase());
+      input = input.replace(/\s/g, '');
+
+      for (var i = 0; i < strings.length; i++) {
+        if (input.indexOf(strings[i]) !== -1) {
+          while (input.indexOf(strings[i]) !== -1) {
+            input = input.replace(strings[i], '');
+          }
+        }
+      }
+
+      if (input.length > 0) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    isLength : function(input, n) {
+      if (!input || !n) {
+        throw "Please fill in field.";
+      }
+
+      var inputLength = input.length;
+
+      if (inputLength <= n) {
+        return true;
+      } else {
+        return false;
+      } 
+      
+    },
+    isOfLength : function(input, n) {
+      if (!input || !n) {
+        throw "Please fill in field.";
+      }
+
+      var inputLength = input.length;
+
+      if (inputLength >= n) {
+        return true;
+      } else {
+        return false;
+      } 
+    },
+    countWords : function(input) {
+      if (input === undefined || input === null) {
+        throw "No input given!";
+      }
+
+      if (input.length === 0 || !input.trim()) {
+        return 0;
+      } else {
+        input = this.withoutSymbols(input);
+        input = input.trim();
+        input = input.split(' ');
+
+        return input.length;
+      }
+    },
+    lessWordsThan : function(input, n) {
+       if ( (input === undefined || input === null) || (n === undefined || n === null) ) {
+        throw "No input given!";
+      }
+
+      var inputLength;
+
+      if (input.length === 0 || !input.trim()) {
+        inputLength = input.length;
+      } else {
+        input = this.withoutSymbols(input);
+        input = input.trim();
+        input = input.split(' ');
+
+        inputLength = input.length;
+       
+      }
+
+      if (inputLength <= n) {
+        return true;
+      } else {
+        return false;
+      }
+
+    },
+    moreWordsThan : function(input, n) {
+      if ( (input === undefined || input === null) || (n === undefined || n === null) ) {
+        throw "No input given!";
+      }
+
+      var inputLength;
+
+      if (input.length === 0 || !input.trim()) {
+        inputLength = input.length;
+      } else {
+        input = this.withoutSymbols(input);
+        input = input.trim();
+        input = input.split(' ');
+
+        inputLength = input.length;
+       
+      }
+
+      if (inputLength >= n) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isBetween : function(input, floor, ceil) {
+      if (input >= floor && input <= ceiling) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isAlphanumeric : function(input) {
+      var match = input.match(/[a-z]|[A-Z]|[0-9]/g);
+
+      if (input.length === 0 || !input.trim()) {
+        return true;
+      }
+      if (match.length === input.length) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isCreditCard : function(input) {
+      var match = input.match(/[A-Z]|[a-z]|[0-9]/g);
+
+      if (match !== null && match.length === 16) {
+        return true;
+      } 
+      if (match !== null && match.length === 16 && input.length === 19 && input.indexOf('-') === 4 && input.indexOf('-') === 9 && input.indexOf('-') === 14 ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isHex : function(input) {
+      var match = input.match(/[0-9]|[A-F]|[a-f]/g);
+
+      if (!input || input === null || input === undefined) {
+        throw "Please input HEX value.";
+      }
+
+      if (input.charAt(0) === '#' && (match.length === 6 || match.length === 3)) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isRGB : function(input) {
+      var match = input.match(/[.\/#!$%\^&\*;:{}=\-_`'"~]/gi);
+      var start = input.startsWith('rgb(');
+      var end = input.endsWith(')');
+      var firstComma = input.indexOf(',');
+      var secondComma = input.lastIndexOf(',');
+      var firstSet = input.substring(4,firstComma);
+      var secondSet = input.substring(firstComma + 1, secondComma);
+      var thirdSet = input.substring(secondComma + 1, input.length - 1);
+
+      if (match) { 
+        return false;
+      }
+      if (start && end && firstComma && secondComma  && (firstSet <= 255 && firstSet.length > 0) && (secondSet <= 255 && secondSet.length > 0) && (thirdSet <= 255 && thirdSet.length > 0) ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isHSL : function(input) {
+      var start = input.startsWith('hsl(');
+      var end = input.endsWith(')');
+      var values = input.slice(4, -1);
+      var valuesArr = values.split(',');
+      var result = false;
+
+      // convert string values to array and number values.
+      for (var i = 0; i < valuesArr.length; i++) {
+        valuesArr[i] = parseFloat(valuesArr[i]);
+      }
+      // check hsl() syntax.
+      // check if first index is between 0-360.
+      // check if index 1 & 2 are between 0-1.
+      if ( start && end && (valuesArr[0] >= 0 && valuesArr[0] <= 360) && (valuesArr[1] >= 0 && valuesArr[1] <= 1) && (valuesArr[2] >= 0 && valuesArr[2] <= 1) ) { 
+        result = true;
+      }
+      console.log(start + valuesArr + end);
+      return result;
+    },
+    isColor : function(input) {
+      if ( this.isHex(input) || this.isRGB(input) || this.isHSL(input) ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isTrimmed : function(input) {
+      var inputArr = input.split(' ');
+      var spaces = [];
+
+      for (var i = 0; i < inputArr.length; i++) {
+        if (inputArr[i] === '') {
+          spaces.push(inputArr[i]);
+        }
+      }
+
+      if (spaces.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  window.validator = validator;
+})(window);
