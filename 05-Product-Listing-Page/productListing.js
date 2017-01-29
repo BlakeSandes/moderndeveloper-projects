40cc388bfda775e1453051fe68ba22fe29b4a2ee
BlakeSandes <stellarshade@gmail.com>
2017-01-29 12:36:42 -0800

    More changes and additions on the Product listing page. Made the main header fixed. Added more functionality to the shopping cart. Not quite done it.

--- 05-Product-Listing-Page/productListing.js
+++ 05-Product-Listing-Page/productListing.js
@@ -1,24 +1,15 @@
 // The shopping cart should have the following functionality to:
 
-// Remain hidden until at least one item has been added
 
-// Toggle between showing and hiding the shopping cart by using a button
 
-// Add items to the shopping cart
 
-// Remove items from the shopping cart
 
-// Change the quantity of an item. A quantity of 0 will remove the item from the cart.
-
-// Give a Total Price calculation that updates when items are added or removed. All prices should be kept within an object and referred to for each calculation.
-
-// Allow coupons/promo codes on total prices as well as on individual items. Make up your own promo codes, such as “BIGSALE” or the like
-
-// Add a promo code for 10% off one item, 15% off all items of a specific type, and 5% off the total order
-
-// Only apply 1 promo code at a time
-
-// Only apply a promo code if it makes the total price less than the total price with the current promo code
+// 5. Change the quantity of an item. A quantity of 0 will remove the item from the cart.
+// 6. Give a Total Price calculation that updates when items are added or removed. All prices should be kept within an object and referred to for each calculation.
+// 7. Allow coupons/promo codes on total prices as well as on individual items. Make up your own promo codes, such as “BIGSALE” or the like
+// 8. Add a promo code for 10% off one item, 15% off all items of a specific type, and 5% off the total order
+  // 9Only apply 1 promo code at a time
+// 9. Only apply a promo code if it makes the total price less than the total price with the current promo code
 
 // forEach function to iterate over nodeLists. 
 var forEach = function(array, callback, scope) {
@@ -28,98 +19,117 @@ var forEach = function(array, callback, scope) {
 };
 
 // Element variables
-var basketWrapper = document.querySelector('.basket-container');
-var basketBtn = document.querySelector('.shopping-basket'),
+var basketWrapper = document.querySelector('.basket-wrapper'),
+basket = document.querySelector('.basket'),
+basketBtn = document.querySelector('.shopping-basket'),
 basketDropDown = document.querySelector('.basket-dropdown'),
 basketItems = document.querySelector('.basket-items'),
 addItem = document.getElementsByClassName('add-item'),
+removeBtn = document.getElementsByClassName('remove'),
 quantityCount = document.querySelector('.quantity-count'),
 productCaption = document.querySelectorAll('[data-caption]'),
-
+basketCount = 0,
 addCount,
 dataCaption,
-dataPrice;
+dataPrice,
+eachBasketFragment;
 
-//************************************************//
+// 2. Toggle between showing and hiding the shopping cart by using a button
+    // Shopping cart individaul item HTML fragment to append to basketDropDown. Once I know templating, this rediculous HTML fragment will go the way of the dodo!
 
-// Shopping cart individaul item HTML fragment to append to basketDropDown. Once I know templating, this rediculous HTML fragment will go the way of the dodo!
+//*********************HTML FRAGMENT*********************//
+function basketFragment(caption,price) {
+  var itemLi = document.createElement('li');
+      captionSpan = document.createElement('span'),
+      priceSpan = document.createElement('span'),
+      amountInput = document.createElement('input'),
+      removeBtnElem = document.createElement('input'),
+      bottomBarDiv = document.createElement('div');
 
-  
-// Create elements outside of event.
+  captionSpan.className = "basket-product-caption";
 
+  priceSpan.className = "price";
+   
+  amountInput.setAttribute('type','text');
+  amountInput.setAttribute('value', '1');
+  amountInput.setAttribute('name', 'amount');
+  amountInput.className = "amount clearfix";
+  amountInput.id = "amount";
+   
+  removeBtnElem.setAttribute('type', 'button');
+  removeBtnElem.setAttribute('name', 'remove');
+  removeBtnElem.setAttribute('value', 'REMOVE');
+  removeBtnElem.className = "remove";
+   
+  bottomBarDiv.className = "bottom-bar clearfix";
 
+  itemLi.appendChild(captionSpan);
+  itemLi.appendChild(priceSpan);
+  itemLi.appendChild(amountInput);
+  itemLi.appendChild(removeBtnElem);
+  itemLi.appendChild(bottomBarDiv);
 
+  // Add the product listing and product price to basket item.
+  captionSpan.innerHTML = caption;
+  priceSpan.innerHTML = "$"+ price +".00";
 
+  return itemLi;
 
-//************************************************//
-  
-  
+};
+ //*********************HTML FRAGMENT*********************//
 
-
-// Show and hide the basket dropdown when shopping bag is clicked.
 basketBtn.addEventListener('click', function(ev) {
   ev.preventDefault();
 
   basketDropDown.classList.toggle('visible');
-  console.log("Click fired");
 }, false);
 
-// Add shopping item to basket when add-item cart icon clicked.
+// 3. Add items to the shopping cart
 forEach(addItem, function(index, elem) {
-  basketCount = 0;
+  
   elem.addEventListener('click', function(ev) {
     ev.preventDefault();
 
-    var itemLi = document.createElement('li');
-    // Create span for where the product caption goes.
-    var captionSpan = document.createElement('span');
-    captionSpan.className = "basket-product-caption";
-    // Create the span where the item price goes.
-    var priceSpan = document.createElement('span');
-    priceSpan.className = "price";
-    // Create the input with changeable item amounts.
-    var amountInput = document.createElement('input');
-    amountInput.setAttribute('type','text'),
-    amountInput.setAttribute('value', '1'),
-    amountInput.setAttribute('name', 'amount'),
-    amountInput.className = "amount clearfix",
-    amountInput.id = "amount";
-    // Create remove button
-    var removeBtn = document.createElement('input');
-    removeBtn.setAttribute('type', 'button'),
-    removeBtn.setAttribute('name', 'remove'),
-    removeBtn.setAttribute('value', 'REMOVE'),
-    removeBtn.className = "remove";
-    // Create bottom bar for stylistic purposes.
-    var bottomBarDiv = document.createElement('div');
-    bottomBarDiv.className = "bottom-bar clearfix";
-    // Append each element to itemLi
-    itemLi.appendChild(captionSpan);
-    itemLi.appendChild(priceSpan);
-    itemLi.appendChild(amountInput),
-    itemLi.appendChild(removeBtn),
-    itemLi.appendChild(bottomBarDiv);
     // Walk up the DOM to grab the product listing.
     dataCaption = this.parentElement.previousElementSibling.previousElementSibling.lastElementChild.dataset.caption;
     // Walk up the DOM to grab the product price.
     dataPrice = this.previousElementSibling.dataset.price;
-    // Add the product listing and product price to basket item.
-    captionSpan.innerHTML = dataCaption;
-    priceSpan.innerHTML = "$"+ dataPrice +".00";
+    
     // Append itemLi HTML fragment to baskeItems ol
-    basketItems.appendChild(itemLi);
+    basketItems.appendChild(basketFragment(dataCaption, dataPrice));
 
+    // 1. Remain hidden until at least one item has been added
+    basketWrapper.classList.add('display');
+    
     // Count up one each time the basket icon is clicked.
     basketCount = basketCount + 1;
     // quantityCount circle will also animate on each click. Briefly flashing and enlargin with the same pink as is in the add-item button. I need to figure that shit out still:)
+
     // Add quantiy to basket quantiy amount circle.
     quantityCount.innerHTML = basketCount;
-    console.log(captionSpan);
-    console.log(dataCaption);
     
+
+    if ( removeBtn.length >= 0) {
+      forEach(removeBtn, function(index, elem) {
+        elem.addEventListener('click', function(ev) {
+          ev.preventDefault();
+          quantityCount.innerHTML = removeBtn.length;
+          basketCount = removeBtn.length;
+          if (removeBtn.length === 0) {
+            basketCount = 0;
+            quantityCount.innerHTML = basketCount;
+          }
+          this.parentElement.remove();
+          
+          
+        }, false);
+      });
+    }
+
   }, false);
 });
 
+// 4. Remove items from the shopping cart
 
 
 
