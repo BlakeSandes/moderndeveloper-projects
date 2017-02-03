0e6e6862ecaaca13e3449fe55a6291e05a407f27
BlakeSandes <stellarshade@gmail.com>
2017-02-03 14:16:25 -0800

    Changed any remaining index.html files to a name that was fitting to the specific project.
    Finally figured out how to update the total when each item changes its value.

--- 05-Product-Listing-Page/productListing.js
+++ 05-Product-Listing-Page/productListing.js
@@ -1,10 +1,5 @@
 // The shopping cart should have the following functionality to:
 
-
-
-
-
-// 5. Change the quantity of an item. A quantity of 0 will remove the item from the cart.
 // 6. Give a Total Price calculation that updates when items are added or removed. All prices should be kept within an object and referred to for each calculation.
 // 7. Allow coupons/promo codes on total prices as well as on individual items. Make up your own promo codes, such as “BIGSALE” or the like
 // 8. Add a promo code for 10% off one item, 15% off all items of a specific type, and 5% off the total order
@@ -18,6 +13,8 @@ var forEach = function(array, callback, scope) {
   }
 };
 
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+
 // Element variables
 var basketWrapper = document.querySelector('.basket-wrapper'),
 basket = document.querySelector('.basket'),
@@ -28,7 +25,12 @@ productCaption = document.querySelectorAll('[data-caption]'),
 basketItems = document.querySelector('.basket-items'),
 removeBtn = document.getElementsByClassName('remove'),
 addItem = document.getElementsByClassName('add-item'),
-itemAmount = document.getElementsByClassName('amount');
+itemAmount = document.getElementsByClassName('amount'),
+basketPrice = document.getElementsByClassName('basket-price'),
+totalPrice = document.querySelector('.total-price'),
+total = 0,
+allPricesInBasket = {};
+
 
 // 2. Toggle between showing and hiding the shopping cart by using a button
     // Shopping cart individaul item HTML fragment to append to basketDropDown. Once I know templating, this rediculous HTML fragment will go the way of the dodo!
@@ -42,9 +44,11 @@ function basketFragment(caption,price) {
       removeBtnElem = document.createElement('input'),
       bottomBarDiv = document.createElement('div');
 
-  captionSpan.className = "basket-product-caption";
+  captionSpan.className = "basket-product-caption",
+  captionSpan.setAttribute('data-basketcaption', caption);
 
-  priceSpan.className = "price";
+  priceSpan.className = "basket-price",
+  priceSpan.setAttribute('data-basketprice', price);
    
   amountInput.setAttribute('type','tel');
   amountInput.setAttribute('value', '1');
@@ -67,12 +71,19 @@ function basketFragment(caption,price) {
 
   // Add the product listing and product price to basket item.
   captionSpan.innerHTML = caption;
-  priceSpan.innerHTML = "$"+ price +".00";
+  priceSpan.innerHTML = "$"+ price;
 
   return itemLi;
 
 };
- //*********************HTML FRAGMENT*********************//
+
+function totalObject(obj) {
+  for (var prop in obj) {
+    total = total + parseFloat(allPricesInBasket[prop]);
+  }
+  totalPrice.innerHTML = "$"+ total.toString();
+  total = 0;
+}
 
 basketBtn.addEventListener('click', function(ev) {
   ev.preventDefault();
@@ -87,12 +98,12 @@ forEach(addItem, function(index, elem) {
     ev.preventDefault();
 
     // Walk up the DOM to grab the product listing.
-    dataCaption = this.parentElement.previousElementSibling.previousElementSibling.lastElementChild.dataset.caption;
+    itemCaption = this.parentElement.previousElementSibling.previousElementSibling.lastElementChild.dataset.caption;
     // Walk up the DOM to grab the product price.
-    dataPrice = this.previousElementSibling.dataset.price;
+    itemPrice = this.previousElementSibling.dataset.itemprice;
     
     // Append itemLi HTML fragment to baskeItems ol
-    basketItems.appendChild(basketFragment(dataCaption, dataPrice));
+    basketItems.insertAdjacentElement('afterbegin',basketFragment(itemCaption, itemPrice));
 
     // 1. Remain hidden until at least one item has been added
     basketWrapper.classList.add('display');
@@ -102,42 +113,66 @@ forEach(addItem, function(index, elem) {
     // Add quantiy to basket quantiy amount circle.
     quantityCount.innerHTML = basketItems.childElementCount;
     
+    // Set the property of the price object to the item caption and value to the item price when they are first added to the basket.
+    allPricesInBasket[itemCaption] = itemPrice;
+    totalObject(allPricesInBasket);
 
+    // Remove items from the shopping cart. Close shopping cart if no items.
     if ( basketItems.childElementCount >= 0) {
+      
       forEach(removeBtn, function(index, elem) {
         elem.addEventListener('click', function(ev) {
           ev.preventDefault();
           this.parentElement.remove();
           quantityCount.innerHTML = basketItems.childElementCount;
           
+          delete allPricesInBasket[this.previousElementSibling.previousElementSibling.previousElementSibling.getAttribute('data-basketcaption')];
+          totalObject(allPricesInBasket);
+
           if (basketItems.childElementCount === 0) {
             basketWrapper.classList.remove('display');
           } 
         }, false);
       });
+
+      // 5. Change the quantity of an item. A quantity of 0 will remove the item from the cart.
       forEach(itemAmount, function(index, elem) {
+
         elem.addEventListener('input', function(ev) {
           ev.preventDefault();
-
+          
+          // Quantity of each item multiplied by cost of item.
+          this.previousElementSibling.innerHTML = "$"+ this.value * this.previousElementSibling.getAttribute('data-basketprice');
+          
+          // Remove the item from the basket if quantity entered is 0.
           if (this.value ==='0') {
             this.parentElement.remove();
             quantityCount.innerHTML = basketItems.childElementCount;
+            // Remove item from total prices object.
+            delete allPricesInBasket[this.previousElementSibling.previousElementSibling.getAttribute('data-basketcaption')];
+            // Update total.
+            totalObject(allPricesInBasket);
           }
+          
+          // Adjust the value in the prices object if amount is greater than 1.
+          if (this.value >= "1") {
+            allPricesInBasket[this.previousElementSibling.previousElementSibling.getAttribute('data-basketcaption')] = this.previousElementSibling.innerHTML.replace('$', '');
+            totalObject(allPricesInBasket);
+          }
+          
+          // Close the basket if there are no items in it.
           if (basketItems.childElementCount === 0) {
             basketWrapper.classList.remove('display');
           } 
         }, false);
       });
     } 
-
   }, false);
 });
 
 
-// 4. Remove items from the shopping cart
 
-
-
+ 
 
 
 
